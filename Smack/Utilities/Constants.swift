04d18916a7c298ff90366cd29bfef73143af0120
Macbook Pro <macbookpro@eches-macbook-pro.local>
2018-07-14 22:38:57 +0100

    started creating the UI for LoginVC

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Eche's Macbook  on 14/07/2018.
+//  Copyright © 2018 Eche's Macbook . All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
