d20c994e865cbf2412941accd164f1591aaa432c
Eche's Macbook <macbookpro@Eches-MacBook-Pro.local>
2018-07-15 12:12:48 +0100

    lesson 4 complete on loginVC set up

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Eche's Macbook  on 15/07/2018.
+//  Copyright © 2018 Eche's Macbook . All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
